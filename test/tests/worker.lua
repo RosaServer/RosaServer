5719f97561f0d53b7b48db3fb47c39dc37f549ed
Mike <44955015+jdbool@users.noreply.github.com>
2020-12-03 09:42:27 -0800

    Only run as many server ticks as needed

--- test/tests/worker.lua
+++ test/tests/worker.lua
@@ -3,7 +3,19 @@ local worker = assert(Worker.new('tests/worker.worker.lua'))
 assert(not worker:receiveMessage())
 worker:sendMessage('hi')
 
-nextTick(function ()
-	local message = assert(worker:receiveMessage())
-	assert(message == 'hello')
-end, 10)
+local maxTicks = 10
+local ticks = 0
+
+local function try ()
+	ticks = ticks + 1
+
+	local message = worker:receiveMessage()
+	if message then
+		assert(message == 'hello')
+	else
+		assert(ticks < maxTicks)
+		nextTick(try)
+	end
+end
+
+nextTick(try)
