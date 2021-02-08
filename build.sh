e51513bec6632abfeb69445cf8315761d16ddb85
noche <micheal4826@tuta.io>
2021-02-08 13:21:08 +0300

    Multicore compiling
    Added -j{nproc} after every make command, could be improved by multipling nproc with * 1.5 because `The 1.5 number is because of the noted I/O bound problem. It is a rule of thumb. About 1/3 of the jobs will be waiting for I/O, so the remaining jobs will be using the available cores. A number greater than the cores is better and you could even go as high as 2x.`

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ cd release
 
 echo -e "${STYLE}Compiling moonjit...${RESET}"
 pushd ../moonjit/src
-	make XCFLAGS+=-DLUAJIT_ENABLE_LUA52COMPAT
+	make XCFLAGS+=-DLUAJIT_ENABLE_LUA52COMPAT -j${nproc}
 popd
 
 echo -e "${STYLE}Compiling RosaServer (${TYPE})...${RESET}"
-cmake -DCMAKE_BUILD_TYPE=${TYPE} ..
-make
+cmake -DCMAKE_BUILD_TYPE=${TYPE} -DOPENSSL_INCLUDE_DIR=/usr/lib -DOPENSSL_SSL_LIBRARY=/usr/lib/libssl.so -DOPENSSL_CRYPTO_LIBRARY=/usr/lib/libcrypto.so ..
+make -j${nproc}
